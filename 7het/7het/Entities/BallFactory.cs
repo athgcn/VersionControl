1cc603863e4a2e8b39a638eb0dde04d40740d706
Lenovo <Lenovo@LAPTOP-J4D1UHDQ>
2022-11-04 10:16:48 +0100

    Labdagyár létrehozása

---
+++ 7het/7het/Entities/BallFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7het.Entities
+{
+    public class BallFactory
+    {
+        //Az osztálynak legyen egy publikus függvénye CreateNew néven Ball visszatérési értékkel
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}

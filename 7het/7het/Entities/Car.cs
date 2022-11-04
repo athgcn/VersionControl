cd9d2c249e1272bab7ab966f57a8f0cbc7701d4f
Lenovo <Lenovo@LAPTOP-J4D1UHDQ>
2022-11-04 11:08:40 +0100

    Autó létrehozása

---
+++ 7het/7het/Entities/Car.cs
@@ -0,0 +1,20 @@
+﻿using _7het.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7het.Entities
+{
+    public class Car : Toy
+    {
+        //Graphics osztály segítségével töltsd be a kisautó képét a Car felületére.
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}

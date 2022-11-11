c0cfef7af5af5012447a0b45512cb87a6f5768b1
Lenovo <Lenovo@LAPTOP-J4D1UHDQ>
2022-11-11 10:39:07 +0100

    VaR kiszámítása

--- VaR/VaR/Form1.cs
+++ VaR/VaR/Form1.cs
@@ -29,6 +29,26 @@ namespace VaR
             //A konstruktorban másold az adattábát a memóriába, majd töltsd fel vele a DataGridView-t!
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
+
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
+
         }
 
         //A függvényben vedd fel az alábbi három részvényt a Portfolio listába a kódminta szerint, majd a portfóliódat jelenítsd megy DataGridView-ban.

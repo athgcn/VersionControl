2d6c5954894e1bb91671639209f363f8b0123c5b
Lenovo <Lenovo@LAPTOP-J4D1UHDQ>
2022-12-02 11:00:00 +0100

    Véletlenszám generálás

--- 9het/9het/Form1.cs
+++ 9het/9het/Form1.cs
@@ -14,15 +14,14 @@ namespace _9het
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random(1234);
+
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
         public Form1()
         {
             InitializeComponent();
-
-            
-
             Population = GetPopulation(@"C:\Temp\nép.csv");
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");

e61da28b30123a98606a293c469673f833aa573d
Lenovo <Lenovo@LAPTOP-J4D1UHDQ>
2022-12-09 10:40:01 +0100

    Iterációs lépés felépítése

--- 10het/10het/Form1.cs
+++ 10het/10het/Form1.cs
@@ -4,31 +4,41 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WorldsHardestGame;
 
-namespace _10het
+namespace week10
 {
-    GameController gc = new GameController();
-    GameArea ga;
-
-    int populationSize = 100;
-    int nbrOfSteps = 10;
-    int nbrOfStepsIncrement = 10;
-    int generation = 1;
-
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+        
         public Form1()
         {
             InitializeComponent();
             ga = gc.ActivateDisplay();
-            this.Controls.Add(ga);
+            Controls.Add(ga);
 
-            gc.AddPlayer();
-            gc.Start(true);
+            gc.GameOver += Gc_GameOver;
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+
+            gc.Start();
         }
+
+        
+        
+
     }
 }
